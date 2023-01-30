f50997ef24250c17b806c786c19f71bd67ad70bb
Dmitry Korolev <chameleonweb2012@gmail.com>
2023-01-30 17:08:33 +0300

    prometheus middleware edit

--- internal/app/api/middleware/prometheus.go
+++ internal/app/api/middleware/prometheus.go
@@ -1,73 +1,45 @@
 package middleware
 
 import (
-	"context"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/rs/zerolog/log"
-	"golang.org/x/sync/errgroup"
 )
+
+var totalRequests = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Number of incoming requests",
+	},
+	[]string{"path"},
+)
+
+var httpDuration = promauto.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Name: "http_response_time_seconds",
+		Help: "Duration of HTTP requests",
+	},
+	[]string{"path", "method"},
+)
+
+var httpCodeCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_response_status_total",
+		Help: "Total number of reponses by HTTP status code.",
+	},
+	[]string{"code", "path", "method"},
+)
+
+func init() {
+	prometheus.Register(totalRequests)
+	prometheus.Register(httpCodeCounter)
+	prometheus.Register(httpDuration)
+}
 
 // Prometheus middleware
 func Prometheus() gin.HandlerFunc {
-
-	var totalRequests *prometheus.CounterVec
-	var httpDuration *prometheus.HistogramVec
-	var httpCodeCounter *prometheus.CounterVec
-
-	g, gCtx := errgroup.WithContext(context.Background())
-
-	g.Go(func() error {
-		totalRequests = prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Name: "http_requests_total",
-				Help: "Number of incoming requests",
-			},
-			[]string{"path"},
-		)
-
-		return prometheus.Register(totalRequests)
-	})
-
-	g.Go(func() error {
-		httpDuration = promauto.NewHistogramVec(
-			prometheus.HistogramOpts{
-				Name: "http_response_time_seconds",
-				Help: "Duration of HTTP requests",
-			},
-			[]string{"path", "method"},
-		)
-
-		return prometheus.Register(httpDuration)
-	})
-
-	g.Go(func() error {
-		httpCodeCounter = prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Name: "http_response_status_total",
-				Help: "Total number of reponses by HTTP status code.",
-			},
-			[]string{"code", "path", "method"},
-		)
-
-		return prometheus.Register(httpCodeCounter)
-	})
-
-	go func() {
-		<-gCtx.Done()
-		totalRequests, httpDuration, httpCodeCounter = nil, nil, nil
-	}()
-
-	if err := g.Wait(); err != nil {
-		log.Error().Err(err).Send()
-		return func(c *gin.Context) {
-			c.Next()
-		}
-	}
-
 	return func(c *gin.Context) {
 		timer := prometheus.NewTimer(httpDuration.WithLabelValues(c.Request.URL.Path, c.Request.Method))
 		totalRequests.WithLabelValues(c.Request.URL.Path).Inc()
